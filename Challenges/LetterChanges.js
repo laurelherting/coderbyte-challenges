26f64726736b6846c1582198f2538257382d8624
Laurel Herting <laurel.herting@gmail.com>
2017-06-02 14:28:23 -0600

    change es5 to es6

--- Challenges/LetterChanges.js
+++ Challenges/LetterChanges.js
@@ -2,16 +2,16 @@
 
 function LetterChanges(str) { 
 
-  var letters = "abcdefghijklmnopqrstuvwxyz"
-      var newArray = [];
+  let letters = "abcdefghijklmnopqrstuvwxyz"
+      let newArray = [];
   
-  for (var i = 0; i < str.length; i++){
+  for (let i = 0; i < str.length; i++){
       
       if(letters.indexOf(str[i]) == -1){
                newArray.push(str[i]);
       } else {
 
-      var letr = letters[letters.indexOf(str[i])+1];
+      let letr = letters[letters.indexOf(str[i])+1];
         if (letr == "a" | letr == "e" | letr == "i" | letr == "o" | letr == "u"){
 
         newArray.push(letr.toUpperCase());
