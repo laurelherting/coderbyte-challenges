96f832d4ca5838df5e7f6058952d170a64fd8aa9
Laurel Herting <laurel.herting@gmail.com>
2017-06-05 17:33:34 -0600

    fix comment

--- Challenges/SimpleSymbols.js
+++ Challenges/SimpleSymbols.js
@@ -1,4 +1,4 @@
-Have the func SimpleSymbols(str) take the str parameter being passed and determine if it is an acceptable sequence by either returning the string true or false. The str parameter will be composed of + and = symbols with several letters between them (ie. ++d+===+c++==a) and for the string to be true each letter must be surrounded by a + symbol. So the string to the left would be false. The string will not be empty and will have at least one letter. 
+/* Have the func SimpleSymbols(str) take the str parameter being passed and determine if it is an acceptable sequence by either returning the string true or false. The str parameter will be composed of + and = symbols with several letters between them (ie. ++d+===+c++==a) and for the string to be true each letter must be surrounded by a + symbol. So the string to the left would be false. The string will not be empty and will have at least one letter. */ 
 
 function SimpleSymbols(str) { 
 
