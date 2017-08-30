b5655a7260dd35a2310771593375cc26295a05ba
Laurel Herting <laurel.herting@gmail.com>
2017-08-30 13:07:28 -0600

    fixed bug

--- Challenges/LetterCapitalize.js
+++ Challenges/LetterCapitalize.js
@@ -1,6 +1,6 @@
 const assert = require('assert');
 /*  Using the JavaScript language, have the function LetterCapitalize(str) take the str parameter being passed and capitalize the first letter of each word. Words will be separated by only one space. */
-function LetterCapitalize(str) { 
+function letterCapitalize(str) { 
   
 // Input:"i like coffee"
 // Output:"I Like Coffee"
@@ -23,14 +23,11 @@ function LetterCapitalize(str) {
   }
   str = arr.join("");
   return str; 
-  // console.log('works1');
 }
 
 const in1 = "i like coffee";
 const expect1 = "I Like Coffee";
-  // console.log('works2');
 const test1 = letterCapitalize(in1);
-   console.log('works');
 assert.strictEqual(test1, expect1, `should be ${expect1}`);
    
 
