42ed70ff926784e4a0a6e0834b742495bb573bf6
Laurel Herting <laurel.herting@gmail.com>
2017-08-31 18:23:08 -0600

    add real example

--- challenges/FirstFactorial.js
+++ challenges/FirstFactorial.js
@@ -1,19 +1,22 @@
+const assert = require('assert');
+
 // Have the function FirstFactorial(num) take the num parameter being passed 
 // return the factorial of it (e.g. if num = 4, return (4 * 3 * 2 * 1))
-// For the test cases, the range will be between 1 and 18 and the input will always be an integer.
+// For the test cases, the range will be between 1 and 18 and the input will
+// always be an integer.
 
 function FirstFactorial(num) { 
-let y = 1
-  for (let i = 1; i <= num; i++){
-  
-    y = y*i
-  
+    if (num === 0) {
+      return 1;
   }
-  
-  num = y
-  // code goes here  
-  return num; 
-         
+  return num * FirstFactorial(num - 1); 
 }
-   
-FirstFactorial();     
+     
+t1 = FirstFactorial(5);                            
+// console.log(t1);
+assert.strictEqual(t1, 120, "Input 5 should yield Output 120");
+
+t2 = FirstFactorial(9);                            
+// console.log(t2);
+assert.strictEqual(t2, 362880, "Input 9 should yield Output 362880");
+
