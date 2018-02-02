4196181c7b170fed0426ae9e0d6e7fcbdf1d3006
Laurel Herting <laurel.herting@gmail.com>
2018-02-02 09:55:14 -0700

    add working tests using .join method

--- Challenges/divisionStringified.js
+++ Challenges/divisionStringified.js
@@ -1,36 +1,32 @@
 const assert = require('assert');
+function DivisionStringified(num1, num2) {
 
-function divisionStringified(num1, num2) {
+    // Divide, then round accordingly 
+    let result = Math.round(num1 / num2).toString(),
 
-// Divide, then round accordingly 
-    let result = Math.round(num1 / num2).toString();
+        // use the .split method to convert the result variable into an array
+        resultArr = result.split("");
 
-    // use the .split method to convert the result variable into an array
-    let resultArr = result.split('');
-
-    // check if the result is greater than 100
-    if (result >= 100) {
+    // check if the result is greater than 1000
+    if (result >= 1000) {
 
     // If so, loop through each item in the array
     // starting at the third character from the end (i = result.length - 3)
     // moving back three charactres each time (i -= 3)
     // stopping at the start of the string (i > 0)
-        for (let i = result.length - 3; i > 0; i -= 3) {
+        for (let i = result.length - 3; i > 0; i -= 3) {  
 
-        // At every third character use the .splice method to insert a comma
-            resultArr.splice(i, 0, ',');
+            // At every third character use the .splice method to insert a comma
+            resultArr.splice(i, 0, ",");
         }
     }
 
-    console.log(resultArr.splice(200,30,4)); //this returns NaN4
-
     // Lastly, return the result array with the .join method to covert it into a string.
-    return resultArr.join('');
+    return resultArr.join("");
 }
 
-console.log(divisionStringified([503 / 52])); // numbers not working
-
-const t1 = "divisionStringified([503, 52])";
+console.log(DivisionStringified(209,35,2));
+const t1 = "DivisionStringified([608, 61])";
 const a1 = "resultArr.splice";
 
 assert(t1);
