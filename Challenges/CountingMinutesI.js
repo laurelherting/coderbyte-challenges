553191e4d4826eedf16484c3ff7c3909e73bb1d3
Laurel Herting <laurel.herting@gmail.com>
2018-02-16 14:47:42 -0700

    fix indent

--- Challenges/CountingMinutesI.js
+++ Challenges/CountingMinutesI.js
@@ -4,35 +4,35 @@ const assert = require('assert');
 Output:1425 */
 function countingMinutes(str) {
 const timeArray = str.split('-');
-  let startTime = timeArray[0];
-  let endTime = timeArray[1];
-  let startTimeArr = startTime.split(':');
-  let startTimeHour = parseInt(startTimeArr[0]);
-  let startTimeMinute = parseInt(startTimeArr[1].replace(/[^0-9]/g, ''));
-  let startTimeAMPM = startTimeArr[1].replace(/[^a-zA-Z]/g, '');
-  let endTimeArr = endTime.split(':');
-  let endTimeHour = parseInt(endTimeArr[0]);
-  let endTimeMinute = parseInt(endTimeArr[1].replace(/[^0-9]/g, ''));
-  let endTimeAMPM = endTimeArr[1].replace(/[^a-zA-Z]/g, '');
+    let startTime = timeArray[0];
+    let endTime = timeArray[1];
+    let startTimeArr = startTime.split(':');
+    let startTimeHour = parseInt(startTimeArr[0]);
+    let startTimeMinute = parseInt(startTimeArr[1].replace(/[^0-9]/g, ''));
+    let startTimeAMPM = startTimeArr[1].replace(/[^a-zA-Z]/g, '');
+    let endTimeArr = endTime.split(':');
+    let endTimeHour = parseInt(endTimeArr[0]);
+    let endTimeMinute = parseInt(endTimeArr[1].replace(/[^0-9]/g, ''));
+    let endTimeAMPM = endTimeArr[1].replace(/[^a-zA-Z]/g, '');
 
-  if ((startTimeHour !== 12) && (startTimeAMPM === 'pm')) {
+    if ((startTimeHour !== 12) && (startTimeAMPM === 'pm')) {
     startTimeHour += 12;
-  }
-  if ((endTimeHour !== 12) && (endTimeAMPM === 'pm')) {
+    }
+    if ((endTimeHour !== 12) && (endTimeAMPM === 'pm')) {
     endTimeHour += 12;
-  }
-  if ((startTimeHour === 12) && (startTimeAMPM === 'am')) {
+    }
+    if ((startTimeHour === 12) && (startTimeAMPM === 'am')) {
     startTimeHour -= 12;
-  }
-  if ((endTimeHour === 12) && (endTimeAMPM === 'am')) {
+    }
+    if ((endTimeHour === 12) && (endTimeAMPM === 'am')) {
     endTimeHour -= 12;
-  }
+    }
 
-  if ((startTimeHour > endTimeHour) || ((startTimeHour === endTimeHour) && (startTimeMinute > endTimeMinute))) {
+    if ((startTimeHour > endTimeHour) || ((startTimeHour === endTimeHour) && (startTimeMinute > endTimeMinute))) {
     return (690 + ((endTimeHour - startTimeHour) * 60) + (endTimeMinute - startTimeMinute));
-  } else {
+    } else {
     return ((endTimeHour - startTimeHour) * 60) + (endTimeMinute - startTimeMinute);
-  }
+    }
 }
 
 console.log('countingMinutes');
