a313f7596ab4d23f03e9c8e5bcc3f858ed445b51
Laurel Herting <laurel.herting@gmail.com>
2018-02-19 09:33:35 -0700

    add function parts

---
+++ Challenges/MeanMode.js
@@ -0,0 +1,15 @@
+const assert = require('assert');
+
+function meanMode (arr) {
+  const modeMap = {};
+  const mode = 0;
+  const modeCount = 0;
+  const sum = 0;
+
+  for (let i = 0; i < arr.length; i++) {
+
+  }
+}
+
+const t1 = meanMode();
+assert.strictEqual(t1);
