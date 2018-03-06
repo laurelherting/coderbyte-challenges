e32b5e92563e5774f107ec8927764fbcba95411f
Laurel Herting <laurel.herting@gmail.com>
2018-03-06 14:29:55 -0700

    add working test

--- Challenges/SwapCase.js
+++ Challenges/SwapCase.js
@@ -1,16 +1,16 @@
 const assert = require('assert');
 
-/*
-input: "Hello-World"
-output: "hELLO-wORLD"
-*/
-function SwapCase(str) {
-  // turn the string into an array
+// Turn the string into an array
+// For example: if the input string is "Hello-World", then your program
+// should return the string "hELLo-wORLD"
+// loop through the array-swap letter cases
+// uppercase -> lowercase
+// lowercase -> uppercase
+// return modified array as a string
+
+function swapCase(str) {
   const chars = str.split('');
 
-  // loop through the array-swap letter cases
-  // uppercase -> lowercase
-  // lowercase -> uppercase
   for (let i = 0; i < chars.length; i += 1) {
     if (chars[i] === chars[i].toUpperCase()) {
       chars[i] = chars[i].toLowerCase();
@@ -19,12 +19,13 @@ function SwapCase(str) {
     }
   }
 
-  // return modified array joined as string
-  return chars.join('');
+  const caseStr = chars.join('');
+  console.log(caseStr);
+  return caseStr;
 }
 
 const in1 = 'Hello-World'; // input
 const expect1 = 'hELLO-wORLD'; // output
-const test1 = SwapCase(in1);
+const test1 = swapCase(in1);
 assert.strictEqual(test1, expect1, `should be ${expect1}`);
-// console.log('SwapCase');
+// console.log('swapCase');
