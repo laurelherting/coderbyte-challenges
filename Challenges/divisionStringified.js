8e51471d2ea73216e986254f8d5fa441fb650d43
Laurel Herting <laurel.herting@gmail.com>
2018-03-16 15:56:27 -0600

    update function

--- Challenges/divisionStringified.js
+++ Challenges/divisionStringified.js
@@ -1,32 +1,32 @@
 const assert = require('assert');
-function DivisionStringified(num1, num2) {
 
-    // Divide, then round accordingly 
-    let result = Math.round(num1 / num2).toString(),
+const DivisionStringified = (num1, num2) => {
 
-        // use the .split method to convert the result variable into an array
-        resultArr = result.split("");
+  // Divide, then round accordingly
+  const result = Math.round(num1 / num2).toString(),
 
-    // check if the result is greater than 1000
-    if (result >= 1000) {
+  // use the .split method to convert the result variable into an array
+    resultArr = result.split('');
+
+  // check if the result is greater than 1000
+  if (result >= 1000) {
 
     // If so, loop through each item in the array
     // starting at the third character from the end (i = result.length - 3)
-    // moving back three charactres each time (i -= 3)
+    // moving back three characters each time (i -= 3)
     // stopping at the start of the string (i > 0)
-        for (let i = result.length - 3; i > 0; i -= 3) {  
+    for (let i = result.length - 3; i > 0; i -= 3) {
 
-            // At every third character use the .splice method to insert a comma
-            resultArr.splice(i, 0, ",");
-        }
+    // At every third character use the .splice method to insert a comma
+      resultArr.splice(i, 0, ',');
     }
+  }
 
-    // Lastly, return the result array with the .join method to covert it into a string.
-    return resultArr.join("");
-}
+  // Lastly, return the result array with the .join method to covert it into a string.
+  return resultArr.join('');
+};
 
-console.log(DivisionStringified(209,35,2));
-const t1 = "DivisionStringified([608, 61])";
-const a1 = "resultArr.splice";
+console.log(DivisionStringified(209, 35, 2));
+const t1 = 'DivisionStringified([608, 61])';
 
 assert(t1);
