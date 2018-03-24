a996e88036ed46ba91aba7614d86322a7ec2a23d
Laurel Herting <laurel.herting@gmail.com>
2018-03-24 14:53:49 -0600

    change string, update to es6/7

--- Challenges/WordCount.js
+++ Challenges/WordCount.js
@@ -1,19 +1,16 @@
 // usage: forms the basis for text ananlysis operations,
 // you can make visualizations (ex. word cloud).
 const assert = require('assert');
-function WordCount(str) {
 
-    // Turn the input string into an array by
-    // passing a single space into the .split method
-    // Return length of new array (answer)
-  return str.split(' ').length;
-}
- 
+const WordCount = str =>
+  // Turn the input string into an array by
+  // passing a single space into the .split method
+  // Return length of new array (answer)
+  str.split(' ').length;
+
 // console.log(WordCount('Hello, happy holidays'));
 
-const a1 = "WordCount";
-const r1 = "React is awesome";
-
-const t1 = "WordCount";
+const a1 = 'React is awesome'; // input
+const t1 = WordCount(a1);
 // console.log(r1);
-assert(t1, r1);
+assert(t1, a1);
