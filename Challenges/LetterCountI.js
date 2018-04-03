f7e84f29d82f95560d03e2ef92be5b59c3afe4fc
Laurel Herting <laurel.herting@gmail.com>
2018-04-03 15:57:38 -0600

    update function

--- Challenges/LetterCountI.js
+++ Challenges/LetterCountI.js
@@ -1,20 +1,19 @@
 const assert = require('assert');
 
-function letterCount(str) {
-    let letters = 0;
-    let alphabet = "abcdefghijklmnopqrstuvwxyzABCDEFGHIJKLMNOPQRSTUVWXYZ";
-    let ar = alphabet.split("");
-    for (let i=0; i<str.length;i++) {
-        if (ar.indexOf(str[i]) > -1) {
-            letters = letters + 1;
-        }
+const letterCount = str => {
+  let letters = 0;
+  const alphabet = 'abcdefghijklmnopqrstuvwxyzABCDEFGHIJKLMNOPQRSTUVWXYZ';
+  const ar = alphabet.split('');
+  for (let i = 0; i < str.length; i += 1) {
+    if (ar.indexOf(str[i]) > -1) {
+      letters += 1;
     }
-    return letters;
-}
-console.log(letterCount("Howdy World!!!1"));
+  }
+  return letters;
+};
+console.log(letterCount('Howdy World!!!1'));
 
-const a1 = "letterCount";
-const r1 = "Hello";
-const t1 = "letterCount";
-
-assert(t1);
+const a1 = 'hello';
+const r1 = '5';
+const t1 = letterCount(a1);
+assert(r1, t1);
