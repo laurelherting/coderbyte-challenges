cb6cedf7aca39d73487e73e03aff136ace8695d2
Laurel Herting <laurel.herting@gmail.com>
2018-04-13 15:11:25 -0600

    add function

--- easy-challenges/WordCount.js
+++ easy-challenges/WordCount.js
@@ -6,8 +6,7 @@ const WordCount = str =>
   // Turn the input string into an array by
   // passing a single space into the .split method
   // Return length of new array (answer)
-  str.split(' ').length;
-
+  str.split('').length;
 // console.log(WordCount('Hello, happy holidays'));
 
 const a1 = 'React is awesome'; // input
---
+++ easy-challenges/changingSequence.js
@@ -0,0 +1,17 @@
+const assert = require('assert');
+
+const ChangingSequence = arr => {
+  if (arr.length < 2) return -1;
+  const increasing = arr[0] < arr[1];
+
+  for (let i = 1; i < arr.length - 1; i += 1) {
+    if (increasing) {
+      if (arr[i] > arr[i + 1]) return i;
+    } else if (arr[i] < arr[i + 1]) return i;
+  }
+
+  return -1;
+};
+
+const t1 = ChangingSequence;
+assert(t1);
