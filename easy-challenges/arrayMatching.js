15af3b5b706617ab526cf8566c2dbb4ed2cef81f
Laurel Herting <laurel.herting@gmail.com>
2018-04-19 17:43:42 -0600

    wip add string

---
+++ easy-challenges/arrayMatching.js
@@ -0,0 +1,25 @@
+const assert = require('assert');
+
+const ArrayMatching = strArr => {
+  const temp = [];
+  const strArr01 = strArr[0].replace(/[[\]]/gi, '').split(',');
+  const strArr02 = strArr[1].replace(/[[\]]/gi, '').split(',');
+  // console.log(strArr1);
+  const len =
+    strArr01.length > strArr02.length ? strArr01.length : strArr02.length;
+  for (let i = 0; i < len; i += 1) {
+    if (strArr01[i] === undefined) {
+      strArr01[i] = 0;
+    } else if (strArr02[i] === undefined) {
+      strArr02[i] = 0;
+    }
+    temp.push(+strArr02[i] + +strArr01[i]);
+  }
+  return temp.join('-');
+};
+
+const a1 = "'[5, 2, 3]', '[2, 2, 3, 10, 6]'"; // input
+const r1 = '7-4-6-10-6'; // output
+
+const t1 = ArrayMatching(a1);
+assert.strictEqual(t1, r1);
