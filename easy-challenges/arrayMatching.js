0a035a8f02379b27d8be434b4f48c55e81777ff7
Laurel Herting <laurel.herting@gmail.com>
2018-04-20 07:57:39 -0600

    add second test

--- easy-challenges/arrayMatching.js
+++ easy-challenges/arrayMatching.js
@@ -23,3 +23,9 @@ const r1 = '7-4-6-10-6'; // output
 
 const t1 = ArrayMatching(a1);
 assert.strictEqual(t1, r1);
+
+const a2 = "'[1, 2, 1]', '[2, 1, 5, 2]'"; // input
+const r2 = '3-3-6-2'; // output
+
+const t2 = ArrayMatching(a2);
+assert.strictEqual(t2, r2);
