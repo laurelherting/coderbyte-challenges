3f8e8213e10d73deb8fd7368f5096fa96ea9b643
Laurel Herting <laurel.herting@gmail.com>
2018-04-23 18:39:20 -0600

    wip add function w/ working test

---
+++ easy-challenges/longestIncreasingSequence.js
@@ -0,0 +1,24 @@
+const assert = require('assert');
+
+const LongestIncreasingSequence = arr => {
+  const LNG = arr.length;
+  let max = 0;
+  for (let i = 0; i < LNG - 1; i += 1) {
+    const subSet = [arr[i]];
+    for (let n = i + 1; n < LNG; n += 1) {
+      if (arr[n] > subSet[subSet.length - 1]) {
+        subSet.push(arr[n]);
+      }
+    }
+    if (subSet.length > max) {
+      max = subSet.length;
+    }
+  }
+  return max;
+};
+
+const a1 = '[9, 9, 4, 2]'; // input
+// output= 1
+const t1 = LongestIncreasingSequence(a1);
+// console.log(r1);
+assert(t1, a1);
