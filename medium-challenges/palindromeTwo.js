7da50be90e0d3b938808971709907db15251d607
Laurel Herting <laurel.herting@gmail.com>
2018-05-16 16:21:36 -0600

    wip-function with array

--- medium-challenges/palindromeTwo.js
+++ medium-challenges/palindromeTwo.js
@@ -3,7 +3,8 @@ const assert = require('assert');
 const PalindromeTwo = str => {
 
   // match only numbers and letters, ignore punctuation
-  let arr = str.replace(/[a-z0-9]+/gi).toLowerCase();
+  let arr = str.replace(/[a-z0-9]+/gi);
+  console.log('true');
 
   // join into string
   let joined = arr.split('').reverse().join('');
@@ -13,8 +14,12 @@ const PalindromeTwo = str => {
 
 };
 
-// console.log(PalindromeTwo("Anne, I vote more cars race Rome-to-Vienna"));
+console.log(PalindromeTwo('Tony - sees Gizmo'));
 
 t1 = PalindromeTwo(8);
 console.log(t1);
 assert(t1);
+
+// Input:'Tony - sees Gizmo'
+
+// Output:"true"
