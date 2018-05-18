6e65cce480164efa11e3b7864b3ea46951730e95
Laurel Herting <laurel.herting@gmail.com>
2018-05-18 16:09:03 -0600

    wip-return statement

--- medium-challenges/palindromeTwo.js
+++ medium-challenges/palindromeTwo.js
@@ -3,18 +3,26 @@ const assert = require('assert');
 const PalindromeTwo = str => {
 
   // match only numbers and letters, ignore punctuation
-  let arr = str.replace(/[a-z0-9]+/gi);
-  console.log('true');
+  let arr = /[^A-Z-z0-9]/g;
 
-  // join into string
-  let joined = arr.split('').reverse().join('');
+  str = arr.toLowerCase().replace(arr, '');
+
+  let len = str.length;
+    for (let i = 0; i < len / 2; i+= 1) {
+        if (str[i] !== str[len - 1 - i]) {
+        // console.log("output");
+            return false;
+        }
+    console.log(PalindromeTwo);
+    }
 
   // check for palindrome
-  return arr === joined;
+    return true;
 
 };
 
-console.log(PalindromeTwo('Tony - sees Gizmo'));
+console.log(PalindromeTwo("Tony - sees Gizmo"));
+// palindromeTwo("Tony - sees Gizmo");
 
 t1 = PalindromeTwo(8);
 console.log(t1);
