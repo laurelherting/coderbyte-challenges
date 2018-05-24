59d2d08c61b266b5d0b4896dd19a8a830694cdb1
Laurel Herting <laurel.herting@gmail.com>
2018-05-23 18:41:39 -0600

    wip-fix palindrome test, edit comment

--- medium-challenges/palindromeTwo.js
+++ medium-challenges/palindromeTwo.js
@@ -6,16 +6,17 @@ const PalindromeTwo = str => {
   let re = /[\W_]/g;
 
   let lowRegStr = str.toLowerCase().replace(re, '');
-  // str.toLowerCase() = "Tony - sees Gizmo".toLowerCase() = "tony - sees gizmo"
-  // str.replace(/[\W_]/g, '') = "tony - sees gizmo".replace(/[\W_]/g, '') = "tonyseesgizmo"
-  // let lowRegStr = "tonyseesgizmo";
+  // str.toLowerCase() = "Anne, I vote more cars race Rome-to-Vienna".toLowerCase() = "anne, i vote more cars race rome to vienna"
+  // str.replace(/[\W_]/g, '') = "anne, i vote more cars race rome-to-vienna".replace(/[\W_]/g, '') = "anneivotemorecarsracerometovienna"
+  // let lowRegStr = "anneivotemorecarsracerometovienna";
 
   return lowRegStr === lowRegStr.split('').reverse().join('');
 }
 
-PalindromeTwo("Tony - sees Gizmo");
-
-const a1 = "Tony - sees Gizmo"; // input
-const r1 = "tonyseesgizmo"; // output
+const a1 = "Anne, I vote more cards race Rome-to-Vienna"; // input
+// const r1 = "anneivotemorecarsracerometovienna"; // output
 const t1 = PalindromeTwo(a1);
-assert.strictEqual(t1, r1, `should be ${r1}`);
+// assert.strictEqual(t1, r1, `should be ${r1}`);
+assert(t1);
+
+console.log(PalindromeTwo("Anne, I vote more cars race Rome-to-Vienna"));
