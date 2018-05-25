4c7840747b5752f00ee35dd12f461fa8095a9f9e
Laurel Herting <laurel.herting@gmail.com>
2018-05-24 20:59:14 -0600

    wip-fix return statement

--- medium-challenges/palindromeTwo.js
+++ medium-challenges/palindromeTwo.js
@@ -4,13 +4,14 @@ const PalindromeTwo = str => {
 
   // match only numbers and letters, ignore punctuation
   let re = /[\W_]/g;
-
+  // console.log(re);
   let lowRegStr = str.toLowerCase().replace(re, '');
   // str.toLowerCase() = "Anne, I vote more cars race Rome-to-Vienna".toLowerCase() = "anne, i vote more cars race rome to vienna"
   // str.replace(/[\W_]/g, '') = "anne, i vote more cars race rome-to-vienna".replace(/[\W_]/g, '') = "anneivotemorecarsracerometovienna"
   // let lowRegStr = "anneivotemorecarsracerometovienna";
-
+  // console.log(lowRegStr);
   return lowRegStr === lowRegStr.split('').reverse().join('');
+console.log(lowRegStr);
 }
 
 const a1 = "Anne, I vote more cards race Rome-to-Vienna"; // input
