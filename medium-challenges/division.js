d71689c2d7ec922acd72b71cc49ffa833201dfb3
Laurel Herting <laurel.herting@gmail.com>
2018-06-01 19:08:24 -0600

    wip-add function

---
+++ medium-challenges/division.js
@@ -0,0 +1,14 @@
+const assert = require('assert');
+
+const division = (num1, num2) => {
+  let a = num1;
+  let b = num2;
+
+  return ();
+
+};
+
+
+const a1 = '36 & num2 = 54'; // input
+const t1 = divsion(a1);
+assert(t1);
