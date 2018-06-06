7b24c8da8c4c96dd2ba21424b4325e65c800e127
Laurel Herting <laurel.herting@gmail.com>
2018-06-06 16:12:05 -0600

    wip add working tests

--- easy-challenges/alphabetSearching.js
+++ easy-challenges/alphabetSearching.js
@@ -4,10 +4,12 @@ const alphabetSearching = str => {
   let array = str.split('');
   for (let i = 97; i <= 122; i+= 1) {
     if (!array.includes(String.fromCharCode(i))) {
+    // console.log(alphabetSearching);
        return false;
     }
   }
 
+  console.log(test1);
   return true;
 };
 
@@ -15,3 +17,8 @@ const in1 = "abcdefghijklmnopqrstuvwxyyyy";
 const expect1 = "false";
 const test1 = alphabetSearching(in1);
 assert.strictEqual(test1, expect1, `should be ${expect1}`);
+
+const in2 = "abc123456kmo";
+const expect2 = "false";
+const test2 = alphabetSearching(in2);
+assert.strictEqual(test2, expect2, `should be ${expect2}`);
