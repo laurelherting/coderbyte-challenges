b8ffd4464b482377a85522bce51748323260a3fd
Laurel Herting <laurel.herting@gmail.com>
2018-06-07 17:31:36 -0600

    wip-add array with string

---
+++ medium-challenges/arrayAddition.js
@@ -0,0 +1,16 @@
+const assert = require('assert');
+
+const arrayAddition = arr => {
+  let max = Math.max.apply();
+  let x = arr.length;
+
+  return true;
+
+
+  return false;
+};
+
+const a1 = '';
+const t1 = 'arrayAddition()';
+
+assert(t1, a1);
