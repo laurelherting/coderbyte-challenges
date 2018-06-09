f5542ffeea3faaccf4e7e3cd75945934e16c1a55
Laurel Herting <laurel.herting@gmail.com>
2018-06-08 18:07:22 -0600

    wip: function with array

--- medium-challenges/arrayAddition.js
+++ medium-challenges/arrayAddition.js
@@ -1,7 +1,8 @@
 const assert = require('assert');
 
 const arrayAddition = arr => {
-  let max = Math.max.apply();
+  let max = Math.max.apply(null, arr);
+  arr.splice(arr.indexOf(max), 1);
   let x = arr.length;
 
   return true;
@@ -10,7 +11,7 @@ const arrayAddition = arr => {
   return false;
 };
 
-const a1 = '';
-const t1 = 'arrayAddition()';
+const a1 = '[5,7,16,1,2]';
+const t1 = arrayAddition(a1);
 
-assert(t1, a1);
+assert(t1);
