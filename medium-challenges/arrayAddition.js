c2e36413d58e1e2954135f27d18ebd3956798e92
Laurel Herting <laurel.herting@gmail.com>
2018-06-11 09:58:23 -0600

    wip: function with array, with comments

--- medium-challenges/arrayAddition.js
+++ medium-challenges/arrayAddition.js
@@ -1,11 +1,20 @@
 const assert = require('assert');
+// take the array of numbers stored in arr and
+// return the string true if any combination of
+// numbers in the array can be added up to equal the
+// largest number in the array, otherwise return the string false
 
-const arrayAddition = arr => {
+
+function arrayAddition = arr => {
   let max = Math.max.apply(null, arr);
   arr.splice(arr.indexOf(max), 1);
   let x = arr.length;
 
-  return true;
+  return sumChecker(arr, max, x);
+
+  function sumChecker = () => {
+
+  }
 
 
   return false;
@@ -15,3 +24,8 @@ const a1 = '[5,7,16,1,2]';
 const t1 = arrayAddition(a1);
 
 assert(t1);
+
+// ex. if arr contains [4, 6, 23, 10, 1, 3], then output = true,
+// bc  4 + 6 + 10 + 3 = 23
+//The array will not be empty, will not contain all the same elements,
+//and may contain negative numbers.
