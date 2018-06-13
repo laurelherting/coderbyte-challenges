a72e8b2022587594773ddd0c94c435ef40e0b246
Laurel Herting <laurel.herting@gmail.com>
2018-06-13 09:32:31 -0600

    wip: function, with working test

--- medium-challenges/arrayAddition.js
+++ medium-challenges/arrayAddition.js
@@ -13,10 +13,13 @@ function ArrayAddition(arr) {
 
   function sumChecker(arr, sum, length) {
     if (sum === 0) {
+
       return true;
     }
     if (sum != 0 && length === 0) {
     return false;
+
+      console.log(sumChecker);
     }
     return sumChecker(arr, sum, length -1) ||
       sumChecker(arr, sum [length - 1], length - 1);
@@ -24,11 +27,15 @@ function ArrayAddition(arr) {
 
 }
 
-const a1 = '[5,7,16,1,2]';
+// const a1 = '[5,7,16,1,2]';
+// const r1 = 31;
 // const t1 = arrayAddition(a1);
+// assert.strictEqual(t1, r1);
 
-assert(a1);
-
+// console.log(t1);
+t1 = ArrayAddition([5, 6, 1, 2 ]);
+console.log(t1);
+assert.strictEqual(t1, 14, "Input [5,6,1,2] should yield Output 14");
 // ex. if arr contains [4, 6, 23, 10, 1, 3], then output = true,
 // bc  4 + 6 + 10 + 3 = 23
 //The array will not be empty, will not contain all the same elements,
