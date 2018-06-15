8744c3ad07734249ead64b96a14980eb86877a04
Laurel Herting <laurel.herting@gmail.com>
2018-06-15 14:13:18 -0600

    wip-function with test

--- medium-challenges/arrayAddition.js
+++ medium-challenges/arrayAddition.js
@@ -5,9 +5,9 @@ const assert = require('assert');
 // largest number in the array, otherwise return the string false
 
 function ArrayAddition(arr) {
-  let max = Math.max.apply(null, arr);
+  const max = Math.max.apply(null, arr);
   arr.splice(arr.indexOf(max), 1);
-  let x = arr.length;
+  const x = arr.length;
 
   return sumChecker(arr, max, x);
 
@@ -16,7 +16,7 @@ function ArrayAddition(arr) {
 
       return true;
     }
-    if (sum != 0 && length === 0) {
+    if (sum != 0 && length !== 0) {
     return false;
 
       console.log(sumChecker);
@@ -24,7 +24,6 @@ function ArrayAddition(arr) {
     return sumChecker(arr, sum, length -1) ||
       sumChecker(arr, sum [length - 1], length - 1);
   }
-
 }
 
 // const a1 = '[5,7,16,1,2]';
@@ -35,8 +34,8 @@ function ArrayAddition(arr) {
 // console.log(t1);
 t1 = ArrayAddition([5, 6, 1, 2 ]);
 console.log(t1);
-assert.strictEqual(t1, 14, "Input [5,6,1,2] should yield Output 14");
+assert.strictEqual(t1, 14, 'Input [5,6,1,2] should yield Output 14');
 // ex. if arr contains [4, 6, 23, 10, 1, 3], then output = true,
 // bc  4 + 6 + 10 + 3 = 23
-//The array will not be empty, will not contain all the same elements,
-//and may contain negative numbers.
+// The array will not be empty, will not contain all the same elements,
+// and may contain negative numbers.
