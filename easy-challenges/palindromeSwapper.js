45c7c9c5acf6ce3e4fa803ad013d1fa0c324eca5
Laurel Herting <laurel.herting@gmail.com>
2018-06-20 14:50:18 -0600

    wip-function using array

---
+++ easy-challenges/palindromeSwapper.js
@@ -0,0 +1,23 @@
+const assert = require('assert');
+
+const PalindromeSwapper = (str) => {
+  let inputArray = str.split('');
+  let strLen = inputArray.length;
+
+  let len = arr.length;
+  for (let i = 0; i=+ 1) {
+    if () {
+      return false;
+    }
+  }
+
+  return true;
+
+};
+
+
+const in1 = 'anna'; // input
+const expect1 = 'anna'; // output
+const test1 = PalindromeSwapper(in1);
+assert.strictEqual(test1, expect1, `should be ${expect1}`);
+// console.log('PalindromeSwapper');
