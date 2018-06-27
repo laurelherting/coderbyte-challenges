3f97e653ef49ea70eb23e46289da1cb4d53bd49a
Laurel Herting <laurel.herting@gmail.com>
2018-06-26 21:00:55 -0600

    wip-function

--- easy-challenges/palindromeSwapper.js
+++ easy-challenges/palindromeSwapper.js
@@ -3,17 +3,27 @@ const assert = require('assert');
 const PalindromeSwapper = (str) => {
   const inputArray = str.split('');
   const strLen = inputArray.length;
-  let palTester = function(arr) {
+  const palTester = (arr) => {
     const len = arr.length;
-    for (let i = 0; i < len; i =+ 1) {
+    for (let i = 0; i < len; i = +1) {
       if (arr[i] !== arr[len - (1 + i)]) {
         return false;
       }
     }
     return true;
   };
-};
 
+  for (let i = 0; i < strLen - 1; i+= 1) {
+    const newArray = Array.from(inputArray);
+    newArray.splice(i, 2, newArray[i + 1], newArray[i]);
+    if (palTester(newArray)) {
+      return newArray.join('');
+    }
+
+    // console.log('PalindromeSwapper');
+  }
+  return -1;
+};
 
 const in1 = 'anna'; // input
 const expect1 = 'anna'; // output
