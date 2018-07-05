1d8b44f1f6925bd527c407c3da12129e0d0c96d6
Laurel Herting <laurel.herting@gmail.com>
2018-07-05 10:11:20 -0600

    function w/ splice, with working test

--- easy-challenges/palindromeSwapper.js
+++ easy-challenges/palindromeSwapper.js
@@ -5,7 +5,7 @@ const PalindromeSwapper = (str) => {
   const strLen = inputArray.length;
   const palTester = (arr) => {
     const len = arr.length;
-    for (let i = 0; i < len; i = +1) {
+    for (let i = 0; i < len; i += 1) {
       if (arr[i] !== arr[len - (1 + i)]) {
         return false;
       }
@@ -13,7 +13,7 @@ const PalindromeSwapper = (str) => {
     return true;
   };
 
-  for (let i = 0; i < strLen - 1; i+= 1) {
+  for (let i = 0; i < strLen - 1; i += 1) {
     const newArray = Array.from(inputArray);
     newArray.splice(i, 2, newArray[i + 1], newArray[i]);
     if (palTester(newArray)) {
@@ -29,4 +29,4 @@ const in1 = 'anna'; // input
 const expect1 = 'anna'; // output
 const test1 = PalindromeSwapper(in1);
 assert.strictEqual(test1, expect1, `should be ${expect1}`);
-// console.log('PalindromeSwapper');
+console.log(test1);
