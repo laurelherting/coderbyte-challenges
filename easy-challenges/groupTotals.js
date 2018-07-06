790a97eb378a40740480c6d49522712c4e948762
Laurel Herting <laurel.herting@gmail.com>
2018-07-06 14:49:31 -0600

    wip-count pairs, add comments of intent to solve

--- easy-challenges/groupTotals.js
+++ easy-challenges/groupTotals.js
@@ -1,3 +1,9 @@
+// Read in strArr parameter containing key:value
+// pairs where the key is a string and the value
+// is an integer
+//This should return a string with new key:value pairs
+//separated by a comma so that each key appears just
+//once with the total values summed up
 const assert = require('assert');
 
 const groupTotals = (strArr) => {
@@ -7,13 +13,17 @@ const groupTotals = (strArr) => {
   let counts = {};
   for (let i = 0; i < pairs.length; i+= 1) {
     if (counts[pairs[i]]) {
-
+      counts[pairs[i][0]] += parseInt(pairs[i][1]);
     }
   }
+ let returnArr = [];
   return();
 };
 
 const a1 = '["Z:0", "A:-1"]'; // input
-const r1 = 'A:-1'; // output
+const r1 = 'A:-1';
+// Above output should return the keys in
+// alphabetical order, but exclude keys that have a value
+// of 0 after being summed up
 const t1 = groupTotals(a1);
 assert(r1, t1);
