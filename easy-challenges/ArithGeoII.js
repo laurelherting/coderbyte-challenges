11d4c36a5f28eeb517c06e7e17c1b537ecddec2a
Laurel Herting <laurel.herting@gmail.com>
2018-07-25 13:39:25 -0600

    wip-arr function to store difference

---
+++ easy-challenges/ArithGeoII.js
@@ -0,0 +1,16 @@
+const assert = require('assert');
+
+const ArithGeoII = (arr) => {
+  for (let i = 2; i < arr.length; i += 1) {
+    if () {
+
+    }
+  }
+};
+
+const a1 = '5, 10, 15'; // input
+const r1 = 'Arithmetic'; //output
+const t1 = ArithGeoII(a1);
+
+// console.log(r1);
+assert(r1, t1);
