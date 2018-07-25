148d88dd99a1785e6bba0c1260e23d97032bc455
Laurel Herting <laurel.herting@gmail.com>
2018-07-25 16:02:50 -0600

    function with working test complete

--- easy-challenges/ArithGeoII.js
+++ easy-challenges/ArithGeoII.js
@@ -1,15 +1,18 @@
 const assert = require('assert');
 
 const ArithGeoII = (arr) => {
-  for (let i = 2; i < arr.length; i += 1) {
-    if () {
-
-    }
+  let math;
+  const number = [(arr[1] - arr[0]), (arr[1] / arr[0])];
+  for (let i = 0; i < arr.length - 2; i += 1) {
+    math = -1;
+    if ((number[0] + arr[i + 1]) === arr[i + 2]) { math = 'Arithmetic'; }
+    if ((number[1] * arr[i + 1]) === arr[i + 2]) { math = 'Geometric'; }
   }
+  return math;
 };
 
 const a1 = '5, 10, 15'; // input
-const r1 = 'Arithmetic'; //output
+const r1 = 'Arithmetic'; // output
 const t1 = ArithGeoII(a1);
 
 // console.log(r1);
