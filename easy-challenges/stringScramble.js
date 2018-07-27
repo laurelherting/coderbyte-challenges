5de13a4f0b1d3295e31ae16346f8639568fba869
Laurel Herting <laurel.herting@gmail.com>
2018-07-26 19:10:24 -0600

    wip-function with parameters

--- easy-challenges/stringScramble.js
+++ easy-challenges/stringScramble.js
@@ -2,9 +2,8 @@ const assert = require('assert');
 
 // loop through string 2
 const StringScramble = (str1, str2) => {
-  for () {
-
-    if (str1(str2)) {
+  for (let i = 0; str.2.length; i += 1) {
+    if (str1.indexOf(str2) === -1) {
       return false;
     }
   }
@@ -18,3 +17,8 @@ const in1 = 'cdore & str2 = coder'; // input
 const r1 = 'true'; // output
 const test1 = StringScramble(in1);
 assert(r1, test1);
+
+const in2 = 'h3llko" & str2 = "hello'; // input
+const r2 = 'false'; // output
+const test2 = StringScramble(in2);
+assert(r2, test2);
