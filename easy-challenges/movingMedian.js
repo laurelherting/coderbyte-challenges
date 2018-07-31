dca8dbdec4905572c7eb99c542f070cf379757ed
Laurel Herting <laurel.herting@gmail.com>
2018-07-31 08:55:20 -0600

    wip-splice

--- easy-challenges/movingMedian.js
+++ easy-challenges/movingMedian.js
@@ -1,18 +1,18 @@
 const assert = require('assert');
 
 const MovingMedian = (arr) => {
-  const N = arr.splice(0, 1);
-  const r = [arr[0]];
+  let N = arr.splice(0, 1);
+  let r = [arr[0]];
   for (let i = 1; i < arr.length; i += 1) {
-    r.push(getMedian() ? i + 1);
+  r.push(getMedian(arr.slice((i+1-N > 0) ? i+1-N : 0, i+1)));
   }
 
-  return r.join('');
+  return r.join(',');
 };
 
 const a1 = '5, 2, 4, 6'; // input
 const r1 = '2,3,4'; // output
 const t1 = MovingMedian(a1);
 
-// console.log(r1);
+console.log(r1);
 assert(r1, t1);
