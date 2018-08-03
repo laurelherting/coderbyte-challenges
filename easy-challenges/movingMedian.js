f3552e501f6ab6530e94886f4a2219e69d973619
Laurel Herting <laurel.herting@gmail.com>
2018-08-03 12:18:15 -0600

    wip-arr.sort function

--- easy-challenges/movingMedian.js
+++ easy-challenges/movingMedian.js
@@ -1,7 +1,7 @@
 const assert = require('assert');
 
 const MovingMedian = (arr) => {
-  let N = arr.splice(0, 1);
+  let N = arr.indexOf(0, 1);
   let r = [arr[0]];
   for (let i = 1; i < arr.length; i += 1) {
     r.push(getMedian(arr.slice((i + 1 - N > 0) ? i + 1 - N : 0, i + 1)));
@@ -9,14 +9,14 @@ const MovingMedian = (arr) => {
   return r.join(',');
 };
 
-function getMedian(arr) {
+const getMedian = (arr) => {
   arr.sort((a, b) => a - b);
 
   if (arr.length % 2 === 0) {
     return (arr[arr.length / 2] + arr[(arr.length / 2) - 1]) / 2;
   }
   return arr[(arr.length - 1) / 2];
-}
+};
 
 const a1 = '5, 2, 4, 6'; // input
 const r1 = '2,3,4'; // output
