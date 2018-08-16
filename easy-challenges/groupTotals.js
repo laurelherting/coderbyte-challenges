0d33a470ba631c4d459c21d8e46e77ae110386ee
Laurel Herting <laurel.herting@gmail.com>
2018-08-16 10:44:30 -0600

    wip-loop inside function

--- easy-challenges/groupTotals.js
+++ easy-challenges/groupTotals.js
@@ -1,22 +1,22 @@
 // Read in strArr parameter containing key:value
 // pairs where the key is a string and the value
 // is an integer
-//This should return a string with new key:value pairs
-//separated by a comma so that each key appears just
-//once with the total values summed up
+// This should return a string with new key:value pairs
+// separated by a comma so that each key appears just
+// once with the total values summed up
 const assert = require('assert');
 
 const groupTotals = (strArr) => {
-  let pairs = strArr.map(function(str) {
+  const pairs = strArr.map((str) {
     return str.split(':');
   });
-  let counts = {};
-  for (let i = 0; i < pairs.length; i+= 1) {
+  const counts = {};
+  for (let i = 0; i < pairs.length; i += 1) {
     if (counts[pairs[i]]) {
       counts[pairs[i][0]] += parseInt(pairs[i][1]);
     }
   }
-  let returnArr = [];
+  const returnArr = [];
   for (let key in counts) {
     if (counts[key] !== 0) {
       returnArr.push(`${key}:${counts[key]}`);
