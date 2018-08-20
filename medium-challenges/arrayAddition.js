f829d5efd2390468c77c22139faa6f19d43cb392
Laurel Herting <laurel.herting@gmail.com>
2018-08-20 12:07:48 -0600

    edit function w/ test

--- medium-challenges/arrayAddition.js
+++ medium-challenges/arrayAddition.js
@@ -5,11 +5,11 @@ const assert = require('assert');
 // largest number in the array, otherwise return the string false
 
 function ArrayAddition(arr) {
-  const max = Math.max.apply(null, arr);
+  const max = Math.max(null, arr);
   arr.splice(arr.indexOf(max), 1);
   const x = arr.length;
 
-  return sumChecker(arr, max, x);
+  return x(arr, max, x);
 
   function sumChecker(arr, sum, length) {
     if (sum === 0) {
@@ -25,15 +25,15 @@ function ArrayAddition(arr) {
   }
 }
 
-// const a1 = '[5,7,16,1,2]';
-// const r1 = 31;
-// const t1 = arrayAddition(a1);
-// assert.strictEqual(t1, r1);
+const a1 = '[5,7,16,1,2]';
+const r1 = 31;
+const t1 = ArrayAddition(a1);
+assert.strictEqual(t1, r1);
 
 // console.log(t1);
-t1 = ArrayAddition([5, 6, 1, 2]);
+// const t1 = ArrayAddition([5, 6, 1, 2]);
+// assert.strictEqual(t1, 14, 'Input [5,6,1,2] should yield Output 14');
 console.log(t1);
-assert.strictEqual(t1, 14, 'Input [5,6,1,2] should yield Output 14');
 // ex. if arr contains [4, 6, 23, 10, 1, 3], then output = true,
 // bc  4 + 6 + 10 + 3 = 23
 // The array will not be empty, will not contain all the same elements,
