fe8df3ad7c13b4285f7f17244321aaf623959ed1
Laurel Herting <laurel.herting@gmail.com>
2018-08-20 12:25:23 -0600

    wip-splice method to change contents of array, edit test

--- medium-challenges/arrayAddition.js
+++ medium-challenges/arrayAddition.js
@@ -6,17 +6,16 @@ const assert = require('assert');
 
 function ArrayAddition(arr) {
   const max = Math.max(null, arr);
-  arr.splice(arr.indexOf(max), 1);
   const x = arr.length;
 
-  return x(arr, max, x);
+  return (arr, max, x);
 
   function sumChecker(arr, sum, length) {
     if (sum === 0) {
       return true;
     }
 
-    // console.log(sumChecker);
+    console.log(sumChecker);
     if (sum === 0 && length !== 0) {
       return false;
     }
@@ -30,9 +29,8 @@ const r1 = 31;
 const t1 = ArrayAddition(a1);
 assert.strictEqual(t1, r1);
 
-// console.log(t1);
-// const t1 = ArrayAddition([5, 6, 1, 2]);
-// assert.strictEqual(t1, 14, 'Input [5,6,1,2] should yield Output 14');
+const t2 = ArrayAddition([5, 6, 1, 2]);
+assert.strictEqual(t2, 14, 'Input [5,6,1,2] should yield Output 14');
 console.log(t1);
 // ex. if arr contains [4, 6, 23, 10, 1, 3], then output = true,
 // bc  4 + 6 + 10 + 3 = 23
