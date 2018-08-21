d3a41ccb827830666a03f1e528e8eca8c703afa8
Laurel Herting <laurel.herting@gmail.com>
2018-08-21 15:04:51 -0600

    prettier fix, edit test, comment out console.logs

--- easy-challenges/AlphabetSoup.js
+++ easy-challenges/AlphabetSoup.js
@@ -1,6 +1,6 @@
 const assert = require('assert');
 
-const AlphabetSoup = str => {
+const AlphabetSoup = (str) => {
   // convert the string into an array of characters
   const chars = str.split('');
 
@@ -14,9 +14,11 @@ const AlphabetSoup = str => {
 const a1 = 'AlphabetSoup';
 const r1 = 'whoosh';
 const t1 = AlphabetSoup(a1);
-console.log(r1);
+// console.log(r1);
 assert(t1, r1);
 
-// const a2 = 'whoosh';
+const a2 = 'whoosh';
 const r2 = 'oohhsw'; // output
-console.log(r2);
+const t2 = AlphabetSoup(a2);
+assert(t2, r2);
+// console.log(r2);
