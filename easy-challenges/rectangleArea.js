3691d10db803669910b6c3440c0f85fbc1cff0ec
Laurel Herting <laurel.herting@gmail.com>
2018-08-22 17:26:14 -0600

    wip-function w/ string match, prettier fix

--- easy-challenges/rectangleArea.js
+++ easy-challenges/rectangleArea.js
@@ -1,7 +1,7 @@
 const assert = require('assert');
 // determine the area formed by the 4 points.
 // The area is the width of the rectangle * the height
-const RectangleArea = strArr => {
+const RectangleArea = (strArr) => {
   const points = strArr.map(str => str.match(/\d+/g));
   const minX = points.map(point => point[0]).sort()[0];
   const minY = points.map(point => point[1]).sort()[0];
@@ -18,6 +18,5 @@ const RectangleArea = strArr => {
 
 const a1 = '"(1 1)","(1 3)","(3 1)","(3 3)"'; // input
 const r1 = '4'; // output
-
 const t1 = RectangleArea(a1);
 assert.strictEqual(t1, r1);
