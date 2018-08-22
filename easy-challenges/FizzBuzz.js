c4ece2b032f5f5a4247ea5a55739e2eb9dbf2ed5
Laurel Herting <laurel.herting@gmail.com>
2018-08-22 09:42:11 -0600

    edit test, comment out console.logs, prettier fix spacing

--- easy-challenges/FizzBuzz.js
+++ easy-challenges/FizzBuzz.js
@@ -1,7 +1,9 @@
 const assert = require('assert');
-// Print out all the numbers from 1 to 100. For every number divisible by 3 print replace it with
-// the word "Fizz", for any number divisible by 5 replace it with the word "Buzz" and for a number divisible
-// by both 3 and 5, replace it with the word "FizzBuzz".
+// Print out all the numbers from 1 to 100. For every
+// number divisible by 3 print replace it with the word "Fizz",
+// for any number divisible by 5 replace it with the word "Buzz"
+// and for a number divisible by both 3 and 5, replace it with
+// the word "FizzBuzz".
 // So, your program should output:
 // 1
 // 2
@@ -15,19 +17,18 @@ const assert = require('assert');
 const fizzBuzz = () => {
   for (let i = 1; i <= 100; i += 1) {
     if (i % 3 === 0 && i % 5 !== 0) {
-      console.log('Fizz');
+    // console.log('Fizz');
     } else if (i % 5 === 0 && i % 3 !== 0) {
-      console.log('Buzz');
+    // console.log('Buzz');
     } else if (i % 3 === 0 && i % 5 === 0) {
-      console.log('FizzBuzz');
+    // console.log('FizzBuzz');
     } else {
-      console.log(i);
+    // console.log(i);
     }
   }
 };
 
-const a1 = fizzBuzz(3, 5);
-const r1 = 'i';
+const a1 = '(3, 5)'; // input
+const r1 = 'i'; // output
 const t1 = fizzBuzz(a1);
-
-assert.strictEqual(t1, r1);
+assert(r1, t1);
