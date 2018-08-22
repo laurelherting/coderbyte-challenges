ec3cbca9179a82693991ff58a1b83b2872be8bc6
Laurel Herting <laurel.herting@gmail.com>
2018-08-22 13:39:34 -0600

    prettier fix, comment out console.logs, add comma

--- easy-challenges/binaryReversal.js
+++ easy-challenges/binaryReversal.js
@@ -1,6 +1,6 @@
 const assert = require('assert');
 
-const BinaryReversal = str => {
+const BinaryReversal = (str) => {
   let num = Number(str).toString(2);
 
   // add leading zeroes to make the number an integral number
@@ -13,10 +13,10 @@ const BinaryReversal = str => {
       .split('')
       .reverse()
       .join(''),
-    2
+    2,
   );
 };
 
 const t1 = BinaryReversal(213); // input
-console.log(t1);
+// console.log(t1);
 assert.strictEqual(t1, 171, 'Input 213 should yield Output 171');
