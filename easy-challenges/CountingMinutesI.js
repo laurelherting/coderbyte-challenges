e5a38a486a8d5f6aabdf37629931ee276c3d31af
Laurel Herting <laurel.herting@gmail.com>
2018-08-23 10:10:03 -0600

    prettier fix, comment out console.logs

--- easy-challenges/CountingMinutesI.js
+++ easy-challenges/CountingMinutesI.js
@@ -1,6 +1,6 @@
 const assert = require('assert');
 
-const countingMinutes = str => {
+const countingMinutes = (str) => {
   const timeArray = str.split('-');
   const startTime = timeArray[0];
   const endTime = timeArray[1];
@@ -13,11 +13,11 @@ const countingMinutes = str => {
   const endTimeMinute = parseInt(endTimeArr[1].replace(/[^0-9]/g, ''), 10);
   const endTimeAMPM = endTimeArr[1].replace(/[^a-zA-Z]/g, '');
 
-  console.log('startTimeHour', startTimeHour);
+  // console.log('startTimeHour', startTimeHour);
   if (startTimeHour !== 12 && startTimeAMPM === 'pm') {
     startTimeHour += 12;
   }
-  console.log('endTimeHour', endTimeHour);
+  // console.log('endTimeHour', endTimeHour);
   if (endTimeHour !== 12 && endTimeAMPM === 'pm') {
     endTimeHour += 12;
   }
@@ -27,8 +27,8 @@ const countingMinutes = str => {
   if (endTimeHour === 12 && endTimeAMPM === 'am') {
     endTimeHour -= 12;
   }
-  console.log('startTimeHour', startTimeHour);
-  console.log('endTimeHour', endTimeHour);
+  // console.log('startTimeHour', startTimeHour);
+  // console.log('endTimeHour', endTimeHour);
   if (
     startTimeHour > endTimeHour ||
     (startTimeHour === endTimeHour && startTimeMinute > endTimeMinute)
@@ -43,7 +43,7 @@ const countingMinutes = str => {
 
 const t1 = countingMinutes('12:30pm-12:00am');
 
-console.log(t1);
+// console.log(t1);
 assert.strictEqual(t1, 690, 'Input 12:30pm-12:00am should yield Output 690');
 
 /*
