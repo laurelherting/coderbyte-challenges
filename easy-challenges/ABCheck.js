f747eed17e76d462930c98e411e723d61c7dd660
Laurel Herting <laurel.herting@gmail.com>
2018-08-24 10:11:08 -0600

    edit test, comment out console.logs

--- easy-challenges/ABCheck.js
+++ easy-challenges/ABCheck.js
@@ -6,6 +6,7 @@ const ABCheck = (str) => {
   return ab;
 };
 
-console.log(ABCheck('aj44ikb'));
-const r1 = 'false';
-assert(r1);
+// console.log(ABCheck('aj44ikb'));
+const r1 = 'false'; // input
+const t1 = ABCheck('aj44ikb'); // output
+assert(r1, t1);
