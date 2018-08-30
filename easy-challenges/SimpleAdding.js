940415b26e347a69bd62af321bceefd49aa382dc
Laurel Herting <laurel.herting@gmail.com>
2018-08-30 16:36:31 -0600

    edit test, remove console.log, fix prettier error

--- easy-challenges/SimpleAdding.js
+++ easy-challenges/SimpleAdding.js
@@ -4,16 +4,16 @@ const assert = require('assert');
 // then program should return 10 because 1 + 2 + 3 + 4 = 10.
 // For the test cases, the parameter num will be any number from 1 to 1000.
 
-const SimpleAdding = num => {
+const SimpleAdding = (num) => {
   let addedNum = num;
   for (let i = 1; i <= num; i += 1) {
     addedNum += i;
   }
   return addedNum;
 };
-const output = SimpleAdding(16);
-// keep this function call here
-// console.log('Output', output);
-const r1 = output;
 
-assert(r1);
+const a1 = '4'; // input
+const r1 = '10'; // output
+const t1 = SimpleAdding(a1);
+
+assert(r1, t1);
