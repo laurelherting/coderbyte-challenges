2a658454f0bd56908ece166930e6af04af8cb72b
Laurel Herting <laurel.herting@gmail.com>
2018-08-31 11:44:16 -0600

    fix prettier error, comment out console.log

--- easy-challenges/Third_greatest.js
+++ easy-challenges/Third_greatest.js
@@ -1,10 +1,10 @@
 const assert = require('assert');
 
-const ThirdGreatest = strArr => {
+const ThirdGreatest = (strArr) => {
   strArr.sort((a, b) => b.length - a.length);
   return strArr[2];
 };
 
-console.log(ThirdGreatest([2, 3, 4, 6, 8]));
+// console.log(ThirdGreatest([2, 3, 4, 6, 8]));
 const t1 = ThirdGreatest;
 assert(t1);
