682d0ea21d974397c8555119255517b1120528ab
Laurel Herting <laurel.herting@gmail.com>
2018-09-05 21:01:59 -0600

    edit test, comment out console.log, remove comment

--- easy-challenges/VowelCount.js
+++ easy-challenges/VowelCount.js
@@ -3,11 +3,11 @@ const assert = require('assert');
 const countVowels = str => Array.from(str)
   .filter(letter => 'aeiou'.includes(letter)).length;
 
-console.log(countVowels('abcdefghijklmnopqrstuvwxyz')); // 5
-console.log(countVowels('December')); // 3
-console.log(countVowels('xxx')); // 0
+// console.log(countVowels('abcdefghijklmnopqrstuvwxyz')); // 5
+// console.log(countVowels('xxx')); // 0
 
-const a1 = "vowels";
-const r1 = "array";
+const a1 = 'September'; // input
+const r1 = '3'; // output
+const t1 = countVowels(a1);
 
-assert(r1);
+assert(r1, t1);
