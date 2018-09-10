cbcb2ae16dee161c2d79b63b669182387ba95683
Laurel Herting <laurel.herting@gmail.com>
2018-09-10 11:52:55 -0600

    edit test, modify function comment, fix prettier error

--- easy-challenges/hammingDistance.js
+++ easy-challenges/hammingDistance.js
@@ -1,7 +1,9 @@
 const assert = require('assert');
-// loop through one of the strings, check if the characters at each position in both strings are equal to each other
+// loop through one of the strings, check if the
+// characters at each position in both strings are
+// equal to each other
 
-const HammingDistance = strArr => {
+const HammingDistance = (strArr) => {
   const word1 = strArr[0];
   const word2 = strArr[1];
   let count = 0;
@@ -18,8 +20,9 @@ const in1 = '10011, 10100'; // input
 const expect1 = '3'; // output
 const test1 = HammingDistance(in1);
 assert(test1, expect1);
-// Input:"10011", "10100"
-// Output:3
 
-// Input:"helloworld", "worldhello"
-// Output:8
+
+const in2 = 'helloworld, worldhello'; // input
+const expect2 = '8'; // output
+const test2 = HammingDistance(in2);
+assert(test2, expect2);
