2b1116a77696018beff3189fcbc82ef039ed9b72
Laurel Herting <laurel.herting@gmail.com>
2018-09-10 14:41:43 -0600

    wip-eslint error, fix test

--- easy-challenges/overlappingRanges.js
+++ easy-challenges/overlappingRanges.js
@@ -1,6 +1,6 @@
 const assert = require('assert');
 
-const OverlappingRanges = arr => {
+const OverlappingRanges = (arr) => {
   let counter = 0;
   for (let i = arr[0]; i < arr[1]; i += 1) {
     if (i >= arr[2] && i <= arr[3]) {
@@ -10,13 +10,12 @@ const OverlappingRanges = arr => {
   return counter >= arr[4];
 };
 
-const a1 = '[1, 8, 2, 4, 4]';
-const r1 = 'true';
-
+const a1 = '[1, 8, 2, 4, 4]'; // input
+const r1 = 'false'; // output
 const t1 = OverlappingRanges(a1);
 assert.strictEqual(t1, r1);
 
-// Input: 5,11,1,5,1
-// Output: true
-// Input: 1,8,2,4,4
-// Output: false
+const a2 = '[5, 11, 1, 5, 1]'; // input
+const r2 = 'true'; // output
+const t2 = OverlappingRanges(a2);
+assert.strictEqual(t2, r2);
