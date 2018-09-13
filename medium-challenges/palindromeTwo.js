7cd61681b05d690b11882f7bddd1e794aebb2c21
Laurel Herting <laurel.herting@gmail.com>
2018-09-13 15:42:05 -0600

    comment out console.log, fix prettier error

--- medium-challenges/palindromeTwo.js
+++ medium-challenges/palindromeTwo.js
@@ -1,11 +1,10 @@
 const assert = require('assert');
 
-const PalindromeTwo = str => {
-
+const PalindromeTwo = (str) => {
   // match only numbers and letters, ignore punctuation
-  let re = /[\W_]/g;
+  const re = /[\W_]/g;
   // console.log(re);
-  let lowRegStr = str.toLowerCase().replace(re, '');
+  const lowRegStr = str.toLowerCase().replace(re, '');
 
   return lowRegStr === lowRegStr.split('').reverse().join('');
 };
@@ -14,5 +13,5 @@ const a1 = 'Anne, I vote more cards race Rome-to-Vienna'; // input
 const r1 = 'anneivotemorecarsracerometovienna'; // output
 const t1 = PalindromeTwo(a1);
 
-console.log(r1);
+// console.log(r1);
 assert(r1, t1);
