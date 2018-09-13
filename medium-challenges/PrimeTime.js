79cb2666277e3ae22bb8400b48f7567277cb0ce4
Laurel Herting <laurel.herting@gmail.com>
2018-09-13 09:53:45 -0600

    prettier fix

--- medium-challenges/PrimeTime.js
+++ medium-challenges/PrimeTime.js
@@ -1,15 +1,16 @@
 const assert = require('assert');
-const PrimeTime = num => {
+
+const PrimeTime = (num) => {
   if (num === 1) {
     return false;
   }
-  for (let i = 2; i < num; i+= 1) {
+  for (let i = 2; i < num; i += 1) {
     if (num % i === 0) {
       return false;
     }
   }
   return true;
-}
+};
 
 const a1 = 'false';
 const t1 = PrimeTime(a1);
