1d379bb4e36a855a4e363ebc0f28eea8c6accdf8
Laurel Herting <laurel.herting@gmail.com>
2018-11-14 13:03:07 -0700

    wip-loop through array

---
+++ easy-challenges/simpleMode.js
@@ -0,0 +1,14 @@
+const assert = require('assert');
+
+
+const SimpleMode = (arr) => {
+  const table = {};
+  for (var i = 0; i < arr.length; i+= 1) {
+    let number = arr[i];
+
+  }
+};
+
+console.log(SimpleMode('5,5,2,2,1'));
+const r1 = '5';
+assert(r1);
