d90a4b5503bf84f06c556406f649813b4dd33324
Laurel Herting <laurel.herting@gmail.com>
2019-07-23 09:41:28 -0600

    wip-add comments for booleans

--- easy-challenges/ArithGeo.js
+++ easy-challenges/ArithGeo.js
@@ -1,10 +1,17 @@
 const assert = require('assert');
 
 const ArithGeo = (arr) => {
+  // declare two empty arrays to hold the differences and
+  // quotients of each item in the input array
   const diff = arr[1] - arr[0];
   const ratio = arr[1] / arr[0];
+  // Next, declare two booleans as "goods" for whether or not array
+  // follows an arithmetic or geometric pattern
+  // Then, turn these false if the input array doesn't follow an arithmetic or
+  // geometric pattern, initialize them to true
   let allRatioAreGood = true;
   let allDiffAreGood = true;
+
   for (let i = 2; i < arr.length; i += 1) {
     allRatioAreGood += arr[i] / arr[i - 1] === ratio;
     allDiffAreGood += arr[i] - arr[i - 1] === diff;
