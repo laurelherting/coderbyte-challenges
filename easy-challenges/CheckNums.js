f70f7fe128128bd5308af8bd1dcb866e5226c3a7
Laurel Herting <laurel.herting@gmail.com>
2019-07-24 11:15:41 -0600

    add comment for objective & solution

--- easy-challenges/CheckNums.js
+++ easy-challenges/CheckNums.js
@@ -1,9 +1,11 @@
+// objective: Have the func CheckNums(num1,num2) take both parameters being
+// passed and return the string true if num2 is greater than num1,
+// otherwise return false
+// If the parameter values are equal to each other, return the string -1
+
+// solution:
 const assert = require('assert');
 
-// Have the func CheckNums(num1,num2) take both parameters being
-// passed and return the string true if num2 is greater than num1,
-// otherwise return the string false.
-// If the parameter values are equal to each other then return the string -1.
 const CheckNums = (num1, num2) => {
   if (num1 === num2) return '-1';
   return (num2 > num1).toString();
