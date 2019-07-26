cdaba96856b7e5af297669ee80d0e654bb6dd20a
Laurel Herting <laurel.herting@gmail.com>
2019-07-26 10:24:59 -0600

    add objective, solution

--- easy-challenges/FirstFactorial.js
+++ easy-challenges/FirstFactorial.js
@@ -1,9 +1,12 @@
-const assert = require('assert');
-
-// Have the function FirstFactorial(num) take the num parameter being passed
+// objective:
+// Take the num parameter being passed,
 // return the factorial of it (e.g. if num = 4, return (4 * 3 * 2 * 1))
 // For the test cases, the range will be between 1 and 18 and the input will
 // always be an integer.
+
+// solution:
+const assert = require('assert');
+
 const FirstFactorial = (num) => {
   if (num === 0) {
     return 1;
