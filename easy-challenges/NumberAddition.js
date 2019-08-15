0cab61b6ba21a0d4e810c3d189186a31f5e34589
Laurel Herting <laurel.herting@gmail.com>
2019-08-15 14:20:17 -0600

    add objective, solution

--- easy-challenges/NumberAddition.js
+++ easy-challenges/NumberAddition.js
@@ -1,19 +1,21 @@
-const assert = require('assert');
-
+// objective:
 //  Use the match function to find matching patterns in a
 //  string and return an array of all matches found
 //  (set to 0 if none are found)
-//  Use the reduce function in the array to return a single value
+//  Use the reduce function in the array to return a single value.
 // Input:"75Number9"
 // Output:84
 
-const NumberAddition = (str) => {
+// solution:
+const assert = require("assert");
+
+const NumberAddition = str => {
   const pattern = /\d*/g;
   const numbers = str.match(pattern);
   let number = 0;
 
   for (let i = 0; i < numbers.length; i += 1) {
-    if (numbers[i] !== ',') {
+    if (numbers[i] !== ",") {
       number += Number(numbers[i]);
     }
   }
@@ -21,7 +23,7 @@ const NumberAddition = (str) => {
 };
 // console.log(NumberAddition('75Number9'));
 
-const in1 = '75Number9'; // input
-const r1 = '84'; // output
+const in1 = "75Number9"; // input
+const r1 = "84"; // output
 const test1 = NumberAddition(in1);
 assert(r1, test1);
