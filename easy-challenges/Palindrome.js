60dec19a8734f0a7d9725f2029fde31ef8b906e3
Laurel Herting <laurel.herting@gmail.com>
2019-08-20 11:27:02 -0600

    add objtive, solution

--- easy-challenges/Palindrome.js
+++ easy-challenges/Palindrome.js
@@ -1,11 +1,17 @@
-const assert = require('assert');
+// objective:
+// Take the str parameter being passed and return the string true if the
+// parameter is a palindrome, (the string is the same forward as it is backward)
+// otherwise return the string false
+
+// solution:
+const assert = require("assert");
 
 // steps explanation
-const Palindrome = (str) => {
-  const init = str.split(' ').join('');
-  const x = init.split('');
+const Palindrome = str => {
+  const init = str.split(" ").join("");
+  const x = init.split("");
   const y = x.reverse();
-  const z = y.join('');
+  const z = y.join("");
 
   if (z === init) {
     return true;
@@ -15,7 +21,7 @@ const Palindrome = (str) => {
 };
 
 // console.log('Pendulum');
-const a1 = 'A weight, suspended from pivot. It can swing freely.';
-const r1 = 'false';
+const a1 = "A weight, suspended from pivot. It can swing freely.";
+const r1 = "false";
 const t1 = Palindrome(a1);
 assert(r1, t1);
