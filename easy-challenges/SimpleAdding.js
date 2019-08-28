85709e2fe34fa1d630b07b550e8fc72be39e0363
Laurel Herting <laurel.herting@gmail.com>
2019-08-28 15:26:57 -0600

    add objtive, solution

--- easy-challenges/SimpleAdding.js
+++ easy-challenges/SimpleAdding.js
@@ -1,10 +1,13 @@
-const assert = require('assert');
+// objective:
 // Have the func SimpleAdding(num) add up all
-// the numbers from 1 to num. ie: if input is 4
-// then program should return 10 because 1 + 2 + 3 + 4 = 10.
+// the numbers from 1 to num.
+// ie: if input is 4 then program should return 10 because 1 + 2 + 3 + 4 = 10.
 // For the test cases, the parameter num will be any number from 1 to 1000.
 
-const SimpleAdding = (num) => {
+// solution:
+const assert = require("assert");
+
+const SimpleAdding = num => {
   let addedNum = num;
   for (let i = 1; i <= num; i += 1) {
     // ...adding i to answer each time
@@ -14,8 +17,8 @@ const SimpleAdding = (num) => {
   return addedNum;
 };
 
-const a1 = '4'; // input
-const r1 = '10'; // output
+const a1 = "4"; // input
+const r1 = "10"; // output
 const t1 = SimpleAdding(a1);
 
 assert(r1, t1);
