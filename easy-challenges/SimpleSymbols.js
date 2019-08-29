8cec1c59b5849f15351d7312f2451ca0b5cd1a89
Laurel Herting <laurel.herting@gmail.com>
2019-08-29 12:53:02 -0600

    add detil to objective

--- easy-challenges/SimpleSymbols.js
+++ easy-challenges/SimpleSymbols.js
@@ -1,16 +1,21 @@
 // objective:
+// Take the str parameter being passed and determine if it is an acceptable sequence by
+// either returning the string true or false. The str parameter will be composed of + and =
+// symbols with several letters between them (ie. ++d+===+c++==a) and for the string
+// to be true each letter must be surrounded by a + symbol. So the string to the left
+// would be false. The string will not be empty and will have at least one letter/
 // catch invalid strings instead of valid ones
 
 // solution:
-const assert = require('assert');
+const assert = require("assert");
 
-const SimpleSymbols = (str) => {
-  if (str[0] >= 'a' || str[str.length - 1] >= 'a') {
+const SimpleSymbols = str => {
+  if (str[0] >= "a" || str[str.length - 1] >= "a") {
     return false;
   }
   for (let i = 1; i < str.length - 1; i += 1) {
-    if (str[i] >= 'a') {
-      if (str[i + 1] !== '+' || str[i - 1] !== '+') {
+    if (str[i] >= "a") {
+      if (str[i + 1] !== "+" || str[i - 1] !== "+") {
         return false;
       }
     }
@@ -21,28 +26,28 @@ const SimpleSymbols = (str) => {
 
 const a = [
   // valid
-  '++b+===+c++==+a++',
-  '+a+a+a+',
-  '+a++a+',
-  '+a+',
+  "++b+===+c++==+a++",
+  "+a+a+a+",
+  "+a++a+",
+  "+a+",
   // invalid
-  '++b+===+c++==a',
-  '+=b+',
-  '+bb+',
-  '+b=+',
-  '+b+b',
-  'b+b+',
+  "++b+===+c++==a",
+  "+=b+",
+  "+bb+",
+  "+b=+",
+  "+b+b",
+  "b+b+"
 ];
 
 const r = /^[+=\d]*\+(?:[a-z]\+[+=\d]*)+$/im;
 
-a.forEach((s) => {
+a.forEach(s => {
   console.log(r.test(s));
 });
 
-SimpleSymbols('++=+f+4=+s+'); // -> true;
+SimpleSymbols("++=+f+4=+s+"); // -> true;
 
-const a1 = ('++=+f+4=+s+'); // input
+const a1 = "++=+f+4=+s+"; // input
 const t1 = SimpleSymbols(a1);
 
 assert(t1);
