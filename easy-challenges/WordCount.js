ae87a53a177cd40970a24387f7be0d047ff87501
Laurel Herting <laurel.herting@gmail.com>
2019-08-30 10:07:38 -0600

    add return statement

--- easy-challenges/WordCount.js
+++ easy-challenges/WordCount.js
@@ -7,12 +7,13 @@
 // solution:
 const assert = require("assert");
 
-const WordCount = str =>
+const WordCount = str => {
   // Turn the input string into an array by
   // passing a single space into the .split method
   // Return length of new array (answer)
-  str.split("").length;
-// console.log(WordCount('Hello, happy holidays'));
+  return str.split("").length;
+  // console.log(WordCount('Hello, happy holidays'));
+};
 
 const a1 = "React is awesome"; // input
 const t1 = WordCount(a1);
