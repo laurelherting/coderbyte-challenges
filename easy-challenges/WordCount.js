3a40e2f07993696cadce7cea92595485a88b26c4
Laurel Herting <laurel.herting@gmail.com>
2019-08-30 09:58:51 -0600

    wip-add objective, edit solution

--- easy-challenges/WordCount.js
+++ easy-challenges/WordCount.js
@@ -1,15 +1,20 @@
+// objective:
 // usage: forms the basis for text ananlysis operations,
 // you can make visualizations (ex. word cloud).
-const assert = require('assert');
+// Take the str string parameter being passed and return the number of
+// words the string contains
+
+// solution:
+const assert = require("assert");
 
 const WordCount = str =>
   // Turn the input string into an array by
   // passing a single space into the .split method
   // Return length of new array (answer)
-  str.split('').length;
+  str.split("").length;
 // console.log(WordCount('Hello, happy holidays'));
 
-const a1 = 'React is awesome'; // input
+const a1 = "React is awesome"; // input
 const t1 = WordCount(a1);
 // console.log(r1);
 assert(t1, a1);
