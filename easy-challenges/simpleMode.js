7d94d272d479ef29ebce0b9a3c14ae9ef4a38d32
Laurel Herting <laurel.herting@gmail.com>
2019-09-03 10:35:22 -0600

    wip-working test, add objective, solution note

--- easy-challenges/simpleMode.js
+++ easy-challenges/simpleMode.js
@@ -1,13 +1,17 @@
-const assert = require('assert');
+// objective:
+// Take the num parameter being passed which will be an integer and return the
+// string true if it's a power of two. If it's not return the string false.
 
+// solution:
+const assert = require("assert");
 
-const SimpleMode = (arr) => {
+const SimpleMode = arr => {
   const table = {};
   for (let i = 0; i < arr.length; i += 1) {
     const number = arr[i];
   }
 };
 
-console.log(SimpleMode('5,5,2,2,1'));
-const r1 = '5';
+// console.log(SimpleMode("5,5,2,2,1"));
+const r1 = "5";
 assert(r1);
