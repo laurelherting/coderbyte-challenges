5f01b740af09a7bb8df6e1c621c6d5bb6444badd
Laurel Herting <laurel.herting@gmail.com>
2019-09-04 17:05:46 -0600

    add objective & solution, comment out console.log(t1)

--- easy-challenges/VowelCount.js
+++ easy-challenges/VowelCount.js
@@ -1,13 +1,19 @@
-const assert = require('assert');
+// objective:
+// Take the str string parameter being passed and
+// return the number of vowels the string contains
 
-const countVowels = str => Array.from(str)
-  .filter(letter => 'aeiou'.includes(letter)).length;
+// solution:
+const assert = require("assert");
+
+const countVowels = str =>
+  Array.from(str).filter(letter => "aeiou".includes(letter)).length;
 
 // console.log(countVowels('abcdefghijklmnopqrstuvwxyz')); // 5
 // console.log(countVowels('xxx')); // 0
 
-const a1 = 'September'; // input
-const r1 = '3'; // output
+const a1 = "September"; // input
+const r1 = "3"; // output
 const t1 = countVowels(a1);
 
+// console.log(t1);
 assert(r1, t1);
