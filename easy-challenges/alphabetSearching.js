b0a3490ad34f45adc7b6646b72fce1c4e8598a2d
Laurel Herting <laurel.herting@gmail.com>
2019-09-04 11:49:51 -0600

    remove extra space

--- easy-challenges/alphabetSearching.js
+++ easy-challenges/alphabetSearching.js
@@ -1,15 +1,15 @@
-const assert = require('assert');
+const assert = require("assert");
 
 // Take the string parameter being passed and return the string
-// true if every letter from alphabet exists in the string. 
+// true if every letter from alphabet exists in the string.
 // Or else, return false.
 
 const alphabetSearching = str => {
-  const array = str.split('');
+  const array = str.split("");
 
-  for (let i = 97; i <= 122; i+= 1) {
+  for (let i = 97; i <= 122; i += 1) {
     if (!array.includes(String.fromCharCode(i))) {
-       return false;
+      return false;
     }
   }
 
@@ -17,8 +17,8 @@ const alphabetSearching = str => {
   return true;
 };
 
-const a1 = 'gbjkbvnienjnvaabc';
-const r1 = 'true';
+const a1 = "gbjkbvnienjnvaabc";
+const r1 = "true";
 const t1 = alphabetSearching(a1);
 
 // console.log(r1);
