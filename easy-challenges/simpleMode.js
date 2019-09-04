e35877d736130d904b744dc9709e9372f4c528e3
Laurel Herting <laurel.herting@gmail.com>
2019-09-04 10:56:18 -0600

    add working test, with yields

--- easy-challenges/simpleMode.js
+++ easy-challenges/simpleMode.js
@@ -8,7 +8,7 @@ function SimpleMode(bstr) {
   return parseInt((bstr + "").replace(/[^01]/gi, ""), 2);
 }
 
-// console.log(SimpleMode("11001"));
+// console.log(SimpleMode("11001")); yields: 25
 
 const r1 = "5";
 assert(r1);
