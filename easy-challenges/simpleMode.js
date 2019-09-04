ace769e139c54a86ba39869cc4cd8e5a44c5e66d
Laurel Herting <laurel.herting@gmail.com>
2019-09-04 10:13:05 -0600

    wip-tests, edit return statement

--- easy-challenges/simpleMode.js
+++ easy-challenges/simpleMode.js
@@ -5,12 +5,10 @@
 const assert = require("assert");
 
 const SimpleMode = arr => {
-  const table = {};
-  for (let i = 0; i < arr.length; i += 1) {
-    const number = arr[i];
-  }
+  return parseInt((arr + "")
+    .replace(/[^01]/gi, ""), 2);
 };
 
-// console.log(SimpleMode("5,5,2,2,1"));
+console.log(SimpleMode("5,5,2,2,1"));
 const r1 = "5";
 assert(r1);
