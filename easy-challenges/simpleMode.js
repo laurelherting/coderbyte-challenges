00c66b1b017cb37d7817a9a913ca68bdcaf7aeed
Laurel Herting <laurel.herting@gmail.com>
2019-09-04 10:32:04 -0600

    add working test, edit function

--- easy-challenges/simpleMode.js
+++ easy-challenges/simpleMode.js
@@ -4,11 +4,11 @@
 // solution:
 const assert = require("assert");
 
-const SimpleMode = arr => {
-  return parseInt((arr + "")
-    .replace(/[^01]/gi, ""), 2);
-};
+function SimpleMode(bstr) {
+  return parseInt((bstr + "").replace(/[^01]/gi, ""), 2);
+}
 
-console.log(SimpleMode("5,5,2,2,1"));
+// console.log(SimpleMode("11001"));
+
 const r1 = "5";
 assert(r1);
