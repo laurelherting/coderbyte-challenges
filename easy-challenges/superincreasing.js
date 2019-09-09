cc1d06367be50d8e2c9b1637952cd70e49bcf5f5
Laurel Herting <laurel.herting@gmail.com>
2019-09-09 10:26:18 -0600

    edit function name

--- easy-challenges/superincreasing.js
+++ easy-challenges/superincreasing.js
@@ -1,6 +1,6 @@
 const assert = require('assert');
 
-const Superincreasing = (arr) => {
+const SuperIncreasing = (arr) => {
   let sum = 0;
   for (let i = 0; i < arr.length; i += 1) {
     if (arr[i] <= sum) {
@@ -14,11 +14,11 @@ const Superincreasing = (arr) => {
 const a1 = '[1, 2, 3, 4]'; // input
 const r1 = 'false'; // output
 
-const t1 = Superincreasing(a1);
+const t1 = SuperIncreasing(a1);
 assert.strictEqual(t1, r1);
 
 // const a2 = '[1, 2, 5, 10]';
 // const r2 = 'true';
 
-// const t2 = Superincreasing(a2);
+// const t2 = SuperIncreasing(a2);
 // assert.strictEqual(t2, r2);
