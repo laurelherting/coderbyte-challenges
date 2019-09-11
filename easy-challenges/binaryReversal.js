a3f43dd299852118f7023b6c6860459b7d042fee
Laurel Herting <laurel.herting@gmail.com>
2019-09-11 10:08:23 -0600

    add objective

--- easy-challenges/binaryReversal.js
+++ easy-challenges/binaryReversal.js
@@ -1,9 +1,12 @@
 // objective:
+// write a program for reversing numbers in binary. For instance,
+// the binary representation of 13 is 1101, and reversing it gives 1011,
+// which corresponds to number 11
 
 // solution:
-const assert = require('assert');
+const assert = require("assert");
 
-const BinaryReversal = (str) => {
+const BinaryReversal = str => {
   let num = Number(str).toString(2);
 
   // add leading zeroes to make the number an integral number
@@ -13,13 +16,13 @@ const BinaryReversal = (str) => {
 
   return parseInt(
     num
-      .split('')
+      .split("")
       .reverse()
-      .join(''),
-    2,
+      .join(""),
+    2
   );
 };
 
 const t1 = BinaryReversal(213); // input
 // console.log(t1);
-assert.strictEqual(t1, 171, 'Input 213 should yield Output 171');
+assert.strictEqual(t1, 171, "Input 213 should yield Output 171");
