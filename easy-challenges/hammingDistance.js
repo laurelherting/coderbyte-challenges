11bbd5d2ceba1e387ae125a6d1c4e1f989c6fdda
Laurel Herting <laurel.herting@gmail.com>
2019-09-13 12:54:22 -0600

    add objective, solution, edit function

--- easy-challenges/hammingDistance.js
+++ easy-challenges/hammingDistance.js
@@ -1,9 +1,11 @@
-const assert = require('assert');
-// loop through one of the strings, check if the
-// characters at each position in both strings are
-// equal to each other
+// objective:
+// Loop through one of the strings, check if the characters at
+// each position in both strings are equal to each other.
 
-const HammingDistance = (strArr) => {
+// solution:
+const assert = require("assert");
+
+function HammingDistance(strArr) {
   const word1 = strArr[0];
   const word2 = strArr[1];
   let count = 0;
@@ -14,15 +16,14 @@ const HammingDistance = (strArr) => {
     }
   }
   return count;
-};
+}
 
-const in1 = '10011, 10100'; // input
-const expect1 = '3'; // output
+const in1 = "10011, 10100"; // input
+const expect1 = "3"; // output
 const test1 = HammingDistance(in1);
 assert(test1, expect1);
 
-
-const in2 = 'helloworld, worldhello'; // input
-const expect2 = '8'; // output
+const in2 = "helloworld, worldhello"; // input
+const expect2 = "8"; // output
 const test2 = HammingDistance(in2);
 assert(test2, expect2);
