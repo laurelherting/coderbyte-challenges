e9c19a17c67ca67689a993abb7ed589b4858a410
Laurel Herting <laurel.herting@gmail.com>
2019-09-17 15:56:46 -0600

    add objective and solution comment

--- easy-challenges/overlappingRanges.js
+++ easy-challenges/overlappingRanges.js
@@ -1,6 +1,9 @@
-const assert = require('assert');
+// objective:
 
-const OverlappingRanges = (arr) => {
+// solution:
+const assert = require("assert");
+
+const OverlappingRanges = arr => {
   let counter = 0;
   for (let i = arr[0]; i < arr[1]; i += 1) {
     if (i >= arr[2] && i <= arr[3]) {
@@ -10,13 +13,13 @@ const OverlappingRanges = (arr) => {
   return counter >= arr[4];
 };
 
-const a1 = '[1, 8, 2, 4, 4]'; // input
-const r1 = 'false'; // output
+const a1 = "[1, 8, 2, 4, 4]"; // input
+const r1 = "false"; // output
 const t1 = OverlappingRanges(a1);
 assert(t1, r1);
-console.log(OverlappingRanges(['1, 8, 2, 4, 4']));
+// console.log(OverlappingRanges(["1, 8, 2, 4, 4"]));
 
-const a2 = '[5, 11, 1, 5, 1]'; // input
-const r2 = 'true'; // output
+const a2 = "[5, 11, 1, 5, 1]"; // input
+const r2 = "true"; // output
 const t2 = OverlappingRanges(a2);
 assert.strictEqual(t2, r2);
