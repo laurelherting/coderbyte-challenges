f5b3fa6140f94b76f9382f9b6137c65b56726f2c
Laurel Herting <laurel.herting@gmail.com>
2019-09-18 09:31:59 -0600

    add objective

--- easy-challenges/palindromeSwapper.js
+++ easy-challenges/palindromeSwapper.js
@@ -1,12 +1,14 @@
 // objective:
+// Given a string, check if the string can be made palindrome
+// by swapping a character only once.
 
 // solution:
-const assert = require('assert');
+const assert = require("assert");
 
-const PalindromeSwapper = (str) => {
-  const inputArray = str.split('');
+const PalindromeSwapper = str => {
+  const inputArray = str.split("");
   const strLen = inputArray.length;
-  const palTester = (arr) => {
+  const palTester = arr => {
     const len = arr.length;
     for (let i = 0; i < len; i += 1) {
       if (arr[i] !== arr[len - (1 + i)]) {
@@ -20,7 +22,7 @@ const PalindromeSwapper = (str) => {
     const newArray = Array.from(inputArray);
     newArray.splice(i, 2, newArray[i + 1], newArray[i]);
     if (palTester(newArray)) {
-      return newArray.join('');
+      return newArray.join("");
     }
 
     // console.log('PalindromeSwapper');
@@ -28,14 +30,14 @@ const PalindromeSwapper = (str) => {
   return -1;
 };
 
-const in1 = 'anna'; // input
-const expect1 = 'anna'; // output
+const in1 = "anna"; // input
+const expect1 = "anna"; // output
 const test1 = PalindromeSwapper(in1);
 assert.strictEqual(test1, expect1, `should be ${expect1}`);
 // console.log(test1);
 
-const in2 = 'kayak'; // input
-const expect2 = 'kayak'; // output
+const in2 = "kayak"; // input
+const expect2 = "kayak"; // output
 const test2 = PalindromeSwapper(in2);
 assert.strictEqual(test2, expect2, `should be ${expect2}`);
 // console.log(test2);
