f1b49c639558587946d68b44619ad27b608327ab
Laurel Herting <laurel.herting@gmail.com>
2019-09-19 10:15:47 -0600

    add objective and solution, edit function

--- easy-challenges/superincreasing.js
+++ easy-challenges/superincreasing.js
@@ -1,18 +1,25 @@
-const assert = require('assert');
+// objective:
+// Determine if every element in an array is greater than the sum of
+// all the previous elements. Solve this by simply looping through the array
+// while keeping a running sum stored in a variable and checking each element in
+// the array against this sum.
 
-const SuperIncreasing = (arr) => {
+// solution:
+const assert = require("assert");
+
+function SuperIncreasing(arr) {
   let sum = 0;
   for (let i = 0; i < arr.length; i += 1) {
     if (arr[i] <= sum) {
-      return 'false';
+      return "false";
     }
     sum += arr[i];
   }
   return true;
-};
+}
 
-const a1 = '[1, 2, 3, 4]'; // input
-const r1 = 'false'; // output
+const a1 = "[1, 2, 3, 4]"; // input
+const r1 = "false"; // output
 
 const t1 = SuperIncreasing(a1);
 assert.strictEqual(t1, r1);
