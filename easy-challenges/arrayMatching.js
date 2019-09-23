1e29b98ecc40b610ffc59cac8aa7d7af3e10ee8a
Laurel Herting <laurel.herting@gmail.com>
2019-09-23 17:57:44 -0600

    update function

--- easy-challenges/arrayMatching.js
+++ easy-challenges/arrayMatching.js
@@ -4,24 +4,24 @@
 // in the other array, then stor this sum in the new array.
 
 // solution:
-const assert = require('assert');
+const assert = require("assert");
 
-const ArrayMatching = (strArr) => {
-  const arr1 = strArr[0].replace(/[[]]/g, '').split(',');
-  const arr2 = strArr[1].replace(/[[]]/g, '').split(',');
+function ArrayMatching(strArr) {
+  const arr1 = strArr[0].replace(/[[]]/g, "").split(",");
+  const arr2 = strArr[1].replace(/[[]]/g, "").split(",");
 
   const long = arr1.length >= arr2.length ? arr1 : arr2;
   const short = long === arr1 ? arr2 : arr1;
   const summed = long.map((n, i) => Number(n) + Number(short[i] || 0));
 
-  return summed.join('-');
-};
+  return summed.join("-");
+}
 
-const a1 = '[5, 2, 3], [2, 2, 3, 10, 6]'; // input
+const a1 = "[5, 2, 3], [2, 2, 3, 10, 6]"; // input
 const t1 = ArrayMatching(a1);
 assert(t1);
 
-const a2 = '[1, 2, 1], [2, 1, 5, 2]'; // input
+const a2 = "[1, 2, 1], [2, 1, 5, 2]"; // input
 const t2 = ArrayMatching(a2);
 assert(t2);
 
