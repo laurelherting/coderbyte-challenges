761f3f5c8ed5baa717986bddcc854a8509f4eeb9
Laurel Herting <laurel.herting@gmail.com>
2019-09-25 13:33:23 -0600

    update function, add line above return statement, condense objective comment

--- easy-challenges/SimpleAdding.js
+++ easy-challenges/SimpleAdding.js
@@ -1,21 +1,21 @@
 // objective:
-// Have the func SimpleAdding(num) add up all
-// the numbers from 1 to num.
+// Have the func SimpleAdding(num) add up all the numbers from 1 to num.
 // ie: if input is 4 then program should return 10 because 1 + 2 + 3 + 4 = 10.
 // For the test cases, the parameter num will be any number from 1 to 1000.
 
 // solution:
 const assert = require("assert");
 
-const SimpleAdding = num => {
+function SimpleAdding(num) {
   let addedNum = num;
   for (let i = 1; i <= num; i += 1) {
     // ...adding i to answer each time
     addedNum += i;
   }
+
   // return answer
   return addedNum;
-};
+}
 
 const a1 = "4"; // input
 const r1 = "10"; // output
