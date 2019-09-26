a533668af74ef9c7af5e46377f16d4a765b3ed4e
Laurel Herting <laurel.herting@gmail.com>
2019-09-26 13:41:28 -0600

    update function, add objective, solution comment

--- medium-challenges/division.js
+++ medium-challenges/division.js
@@ -1,11 +1,12 @@
-const assert = require('assert');
+// objective:
+// Take both parameters being passed and return the Greatest Common Factor
+// (return the greatest number that evenly goes into both numbers with no
+// remainder) Euclidean algorithm
 
-// take both parameters being passed and
-// return the Greatest Common Factor
-// (return the greatest number that evenly
-// goes into both numbers with no remainder)
-// Euclidean algorithm
-const division = (num1, num2) => {
+// solution:
+const assert = require("assert");
+
+function division(num1, num2) {
   // set up variables
   let a = num1;
   let b = num2;
@@ -19,16 +20,16 @@ const division = (num1, num2) => {
   }
 
   // return the non-zero answer
-  return (a === 0) ? b : a;
-};
+  return a === 0 ? b : a;
+}
 
-const a1 = '7 & num2 = 3'; // input
-const r1 = '1'; // output
+const a1 = "7 & num2 = 3"; // input
+const r1 = "1"; // output
 const t1 = division(a1);
 assert(r1, t1);
 // console.log(division(188));
 
-const a2 = '36 & num2 = 54'; // input
-const r2 = '18'; // output
+const a2 = "36 & num2 = 54"; // input
+const r2 = "18"; // output
 const t2 = division(a2);
 assert(r2, t2);
--- medium-challenges/palindromeTwo.js
+++ medium-challenges/palindromeTwo.js
@@ -1,16 +1,31 @@
-const assert = require('assert');
+// objective:
+// Determine if a string is a palindrome. A palindrome is a string
+// that is read the same forwards as it is backwards, for example "mom" or
+// "eye." This challenge, as opposed to the easier palindrome challenge on
+// Coderbyte, may contains punctuation and spaces which makes it slightly harder.
+// To solve this challenge, use some code written in the "easy-challenges"
+// which required you to reverse a string.
 
-const PalindromeTwo = (str) => {
+// solution:
+const assert = require("assert");
+
+const PalindromeTwo = str => {
   // match only numbers and letters, ignore punctuation
   const re = /[\W_]/g;
   // console.log(re);
-  const lowRegStr = str.toLowerCase().replace(re, '');
+  const lowRegStr = str.toLowerCase().replace(re, "");
 
-  return lowRegStr === lowRegStr.split('').reverse().join('');
+  return (
+    lowRegStr ===
+    lowRegStr
+      .split("")
+      .reverse()
+      .join("")
+  );
 };
 
-const a1 = 'Anne, I vote more cards race Rome-to-Vienna'; // input
-const r1 = 'anneivotemorecarsracerometovienna'; // output
+const a1 = "Anne, I vote more cards race Rome-to-Vienna"; // input
+const r1 = "anneivotemorecarsracerometovienna"; // output
 const t1 = PalindromeTwo(a1);
 
 // console.log(r1);
