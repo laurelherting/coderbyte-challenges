4668574c6ad67902ea15425b350b81f0cefaf68c
Laurel Herting <laurel.herting@gmail.com>
2022-02-20 14:11:36 -0500

    2nd solution

---
+++ easy-challenges/DashInsert2.js
@@ -0,0 +1,21 @@
+// objective:
+// insert a dash between two CONSECUTIVE odd numbers
+
+// solution:
+function DashInsert(str) {
+  var idk = 0;
+  // loop through to find odd numbers
+  while (idx < str.length - 1) {
+    // check if current number is odd and the next number is also odd
+    if (Number(str[idk]) % 2 === 1 && Number(str[idk + 1]) % 2 === 1) {
+      // if it is, then add dash between the two odd numbers
+
+      str = str.slice(0, idk + 1) + "-" + str.slice(idk + 1);
+      idx = idk + 2;
+    } else {
+      idk++;
+    }
+  }
+
+  return str;
+}
