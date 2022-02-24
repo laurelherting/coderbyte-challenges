8fe582f229e715c1d9fb418e08e881c0ddc2dbfa
Laurel Herting <laurel.herting@gmail.com>
2022-02-24 11:11:15 -0500

    wip-test

--- medium-challenges/DistinctList.js
+++ medium-challenges/DistinctList.js
@@ -1,6 +1,8 @@
 /* Take array of numbers - duplicate entries. For example if the input is [1, 2, 2, 2, 3]
 then your program should output 2 because there are two duplicates of one of the elements. */
 
+const assert = require("assert");
+
 function DistinctList(arr) {
   var duplicates = 0;
   for (var i = 0; i < arr.length; i++) {
@@ -22,5 +24,9 @@ function DistinctList(arr) {
   return duplicates;
 }
 
-// console.log(DistinctList(""));
-// "0"
+/* const in1 = "12223"; // input
+const expect1 = "2"; // output
+const test1 = DistinctList(in1);
+console.log(test1);
+assert.strictEqual(test1, expect1, `should be ${expect1}`);
+*/
